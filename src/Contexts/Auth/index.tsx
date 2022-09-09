6a9c5c43612761ce6421dd701d6910f4e62748e9
Benjamin Gordon <ben@Benjamins-MacBook-Pro.local>
2022-09-09 18:11:50 -0500

    added username and userid to auth context

--- src/Contexts/Auth/index.tsx
+++ src/Contexts/Auth/index.tsx
@@ -1,22 +1,47 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState } from "react";
 
-interface IAuthContext{
-  jwt: string
-  updateJwt: (newJwt:string) => void
+interface IAuthContext {
+  jwt: string;
+  updateJwt: (newJwt: string) => void;
+
+  username: string;
+  updateUsername: (newUsername: string) => void;
+
+  userId: number;
+  updateUserId: (newUserId: number) => void;
 }
 
-export const AuthContext = createContext<IAuthContext>({jwt:"", updateJwt: ()=>{}});
+export const AuthContext = createContext<IAuthContext>({
+  jwt: "",
+  updateJwt: () => {},
+  username: "",
+  updateUsername: () => {},
+  userId: -1,
+  updateUserId: () => {},
+});
 
 export const AuthProvider = (props: React.PropsWithChildren<{}>) => {
-  const [jwt, setJwt] = useState("")
+  const [jwt, setJwt] = useState("");
+  const [username, setUsername] = useState("");
+  const [userId, setUserId] = useState(-1);
+
   const updateJwt = (newJwt: string) => {
-    console.log('updated global jwt' + newJwt)
-    setJwt(newJwt)
-  }
+    setJwt(newJwt);
+  };
+
+  const updateUsername = (newUsername: string) => {
+    setUsername(newUsername);
+  };
+
+  const updateUserId = (newUserId: number) => {
+    setUserId(newUserId);
+  };
 
   return (
-    <AuthContext.Provider value={{jwt, updateJwt }}>
+    <AuthContext.Provider
+      value={{ jwt, updateJwt, username, updateUsername, userId, updateUserId }}
+    >
       {props.children}
     </AuthContext.Provider>
-  )
-}
+  );
+};
