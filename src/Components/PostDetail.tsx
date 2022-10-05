7f14e87da2200bfcd06368142da7fbe733ef6656
Benjamin Gordon <ben@Benjamins-MacBook-Pro.local>
2022-10-04 23:39:42 -0500

    fixed image scaling on detail view

--- src/Components/PostDetail.tsx
+++ src/Components/PostDetail.tsx
@@ -19,7 +19,7 @@ const PostDetail = (props: IPostProps) => {
   const navigate = useNavigate();
   return (
     <div
-      className="absolute top-0 left-0 z-10 w-screen h-screen backdrop-blur-sm backdrop-brightness-50 flex items-center justify-center"
+      className="fixed top-0 left-0 z-10 w-screen h-screen backdrop-blur-sm backdrop-brightness-50 flex items-center justify-center"
       onClick={() => props.close()}
     >
       <div
@@ -29,23 +29,23 @@ const PostDetail = (props: IPostProps) => {
         <div className="flex justify-center items-center border-b border-gray-200">
           <h3 className="flex items-center text-lg p-2">{title}</h3>
         </div>
-        <div className="flex flex-row h-full">
+        <div className="flex flex-row justify-stretch h-full">
           <div className="flex justify-center items-center bg-black ">
             <img
               src={content}
               alt="user uploaded content"
               loading="lazy"
-              className="max-h-[900px]"
+              className="h-0 min-h-full"
             />
           </div>
-          <div className="flex flex-col justify-between items-left w-1/3">
-            <a
+          <div className="flex flex-col justify-between items-left w-1/3 p-2">
+            <button
               onClick={() => navigate(`/user/${author}`)}
               className="flex flex-row items-center"
             >
               <UserCircleIcon className="w-8 h-8 hover:cursor-pointer " />
               <p className="pl-1 hover:cursor-pointer">{author}</p>
-            </a>
+            </button>
             <p className="mt-2 pl-2 text-sm">{description}</p>
             <div className="flex justify-self-end">
               <ul>
