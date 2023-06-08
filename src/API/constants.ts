4901d9af65c672f937864606abd878280c0088d0
benjaminwgordon <benjaminwillardgordon@gmail.com>
2023-06-08 17:10:45 -0500

    adds foreign root url for prod deploys

--- src/API/constants.ts
+++ src/API/constants.ts
@@ -1,4 +1,6 @@
 const PROD = process.env.NODE_ENV === "prod";
+console.log("process_env: ", process.env.NODE_ENV);
+
 console.log("root url: ", process.env.PROD_API_ROOT_URL);
 const constants = {
   baseURL: PROD ? process.env.PROD_API_ROOT_URL : "http://localhost:3333",
