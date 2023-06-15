0366680ad87719235292d8f03a627a53b1723b19
benjaminwgordon <benjaminwillardgordon@gmail.com>
2023-06-14 22:11:32 -0500

    corrected conditional baseAPIURL

--- src/API/constants.ts
+++ src/API/constants.ts
@@ -1,9 +1,7 @@
 const PROD = process.env.NODE_ENV === "production";
 console.log("root api url: ", process.env.REACT_APP_PROD_API_ROOT_URL);
 const constants = {
-  baseURL: PROD
-    ? process.env.REACT_APP_PROD_API_ROOT_URL
-    : "http://localhost:3333",
+  baseURL: process.env.REACT_APP_PROD_API_ROOT_URL || "http://localhost:3333",
 };
 
 export default constants;
